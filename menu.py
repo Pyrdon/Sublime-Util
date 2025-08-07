b340014550362c8a151bdfd31a4d42e8f927c30d
Pyrdon <pyrdon@gmail.com>
2025-08-07 22:55:48 +0200

    Addding support for menu handling through the quick panel

---
+++ menu.py
@@ -0,0 +1,204 @@
+import logging
+_logger = logging.getLogger(__name__)
+
+from .util import menu
+from . import misc
+from . import user_input
+
+import sublime
+from collections import namedtuple
+
+# Some named tuples for the menu contents to simplfy implementation
+_MenuContents = namedtuple('MenuContents', ['title', 'description', 'annotation'])
+_MenuItemContents = namedtuple('MenuItemContents', ['title', 'description', 'annotation', 'callback'])
+
+class QuickMenu(menu.Menu):
+    """
+    A class representing a menu of items (other menus or actions)
+    """
+
+    def __init__(self, title = "Top level", description = None, annotation = ""):
+        """
+        Initializes the QuickMenu
+
+        :param title: The title
+        :param description: The description (shown below the title)
+        :param annotation: The annotation (shown to the right)
+        """
+
+        super().__init__(_MenuContents(title, description, annotation))
+
+    def add_menu(self, title, description = None, annotation = ""):
+        """
+        Adds a sub menu
+
+        :param title: The title
+        :param description: The description (shown below the title)
+        :param annotation: The annotation (shown to the right)
+
+        :returns: The sub QuickMenu
+        """
+
+        contents = QuickMenu(title, description, annotation)
+        super().add(contents)
+        return contents
+
+    def add_callback(self, title, description = "", annotation = "", *, callback):
+        """
+        Adds an item with a callback
+
+        :param title: The title
+        :param description: The description (shown below the title)
+        :param annotation: The annotation (shown to the right)
+        :param callback: The callback to be run to apply the action
+        """
+
+        contents = _QuickItem(title, description, annotation, callback)
+        super().add(contents)
+
+    def add_input(self, title, input_caption, input_initial_text, description = "", annotation = "", *, input_callback):
+        """
+        Adds an item with a request for user input
+
+        :param title: The title of the item
+        :param input_caption: The caption of the user input request
+        :param input_initial_text: The intitial text to show in the user input request
+        :param description: The description of the item (shown below the title)
+        :param annotation: The annotation of the item (shown to the right)
+        :param input_callback: The callback to be run to validate and apply the results of the
+                               given input.
+        """
+
+        def _item_callback(item):
+            def _on_cancel():
+                item.back()
+
+            user_input.show_input(
+                caption = input_caption,
+                initial_text = input_initial_text,
+                on_done = input_callback,
+                on_cancel = _on_cancel
+            )
+
+        contents = _QuickItem(title, description, annotation, _item_callback)
+        super().add(contents)
+
+    def add_command(self, title, description = "", annotation = "", *, command):
+        """
+        Adds a Sublime command as an item in the Menu
+
+        :param title: The title
+        :param description: The description (shown below the title)
+        :param annotation: The annotation (shown to the right)
+        :param command: The Command class or name
+        """
+
+        def _command_callback(item):
+            if isinstance(command, str):
+                name = command
+            else:
+                name = misc.class_name_to_command(command)
+
+            _logger.debug(f"Executing command '{name}'.")
+            sublime.run_command(name)
+
+        contents = _QuickItem(title, description, annotation, _command_callback)
+        super().add(contents)
+        return contents
+
+    def execute(self):
+        """
+        Opens the QuickMenu
+        """
+
+        _logger.debug(f"Showing QuickMenu {self}, selecting index {self.selected_index}")
+        items = []
+        for sub_item in self.items:
+            items.append(
+                sublime.QuickPanelItem(
+                    trigger = sub_item.contents.title,
+                    details = sub_item.contents.description,
+                    annotation = sub_item.contents.annotation
+                )
+            )
+
+        sublime.active_window().show_quick_panel(
+            items = items,
+            selected_index = self.selected_index,
+            on_select = self._on_select,
+            on_highlight = self._on_highlight
+        )
+
+    def _on_select(self, index):
+        """
+        Run when user applies an item (presses Enter)
+
+        :param index: The index selected (-1 if cancelling)
+        """
+
+        if index != -1:
+            self.enter()
+        else:
+            self.back()
+
+    def _on_highlight(self, index):
+        """
+        Run when user changes selection and then updates index of selected item
+
+        :param index: The selected index
+        """
+
+        self.select(index)
+
+class _QuickItem(menu.MenuItem):
+    """
+    An internal-only class representing a menu item (action)
+    """
+
+    def __init__(self, title, description, annotation, callback):
+        """
+        Initializes the menu item
+
+        :param title: The title
+        :param description: The description (shown below the title)
+        :param annotation: The annotation (shown to the right)
+        """
+
+        super().__init__(_MenuItemContents(title, description, annotation, callback))
+
+    @property
+    def title(self):
+        """
+        Property reflecting the title of the item
+
+        :returns: The title
+        """
+
+        return self.contents.title
+
+    @property
+    def description(self):
+        """
+        Property reflecting the description of the item
+
+        :returns: The description
+        """
+        return self.contents.description
+
+    @property
+    def annotation(self):
+        """
+        Property reflecting the annotation of the item
+
+        :returns: The annotation
+        """
+
+        return self.contents.annotation
+
+    def execute(self):
+        """
+        Applies the action of the selected item
+        """
+
+        _logger.debug(f"Executing QuickMenu item {self}.")
+        self.contents.callback(self)
