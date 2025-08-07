380e703bfb34276b62f500d64959b75de110f611
Pyrdon <pyrdon@gmail.com>
2025-08-07 22:30:47 +0200

    Added utility function to get Sublime command name from class

--- misc.py
+++ misc.py
@@ -1,4 +1,5 @@
 import time
+from .util import misc
 
 class time_this:
   def __init__(self,txt=''):
@@ -20,3 +21,19 @@ class time_this:
     cs,cms = divmod(cpu_diff*1000,1000)
 
     print("{}{:.0f}s, {:.0f}ms ({:.0f}s, {:.0f}ms in CPU time)".format(self.txt,s,ms,cs,cms))
+
+def class_name_to_command(cls):
+    """
+    Converts a Command class to the Sublime command name
+
+    :param cls: The Command class
+    :raises ValueError: Raised if Command class name does not end with 'Command'
+    :returns: The command name
+    """
+
+    name = cls.__name__
+    suffix = 'Command'
+    if name.endswith(suffix):
+        return misc.camel_to_snake(name.removesuffix(suffix))
+
+    raise ValueError(f"Command class ({name}) name must end with '{suffix}'.")
