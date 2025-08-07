8307f76631ae44dd1714fa192c0cf41e2c696d0b
Pyrdon <pyrdon@gmail.com>
2025-08-07 22:21:29 +0200

    Added several project utility functions
    * Made it possible to get name of a project specified in a project file
    * Made it possible to switch to a project
    * Made it possible to open a project

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import sublime
 import sublime_plugin
+import subprocess
 import os
 
 import logging
@@ -7,17 +8,21 @@ _logger = logging.getLogger(__name__)
 
 from .util import json as json_util
 
-def get_name():
+def get_name(path = None):
     """
-    Gets the project name of the current window
+    Gets the project name of the current window or a specific sublime-project file
 
-    :returns: The project name, None if no project
+    :param path: Sublime-project file or None if referring to open project in current window
+    :returns: The project name, None if no project or file not found
     """
 
-    try:
-        return sublime.active_window().extract_variables()['project_base_name']
-    except KeyError as e:
-        return None
+    if path is None:
+        try:
+            return sublime.active_window().extract_variables()['project_base_name']
+        except KeyError as e:
+            return None
+
+    return os.path.splitext(os.path.basename(path))[0]
 
 def rename(name):
     """
@@ -61,5 +66,24 @@ def rename(name):
         new_ws_path, f"{current_name}.sublime-project", f"{name}.sublime-project")
 
     # Open new project
-    _logger.debug(f"Opening project '{name}'.")
-    win.run_command("open_project_or_workspace", {"file": new_prj_path})
+    switch(new_prj_path)
+
+def switch(path):
+    """
+    Switches to a project of a certain path
+
+    :param path: The project to switch to
+    """
+
+    _logger.debug(f"Switch to project '{get_name(path)}' from path {path}.")
+    sublime.active_window().run_command("open_project_or_workspace", {"file": path})
+
+def open_project(path):
+    """
+    Opens a project of a certain path
+
+    :param path: The project to open
+    """
+
+    _logger.debug(f"Opening project '{get_name(path)}' from path {path}.")
+    subprocess.Popen([sublime.executable_path(), path])
